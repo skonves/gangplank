394721fe2165553d7799903bf8a31518fafeaa36
Steve Konves <stephen.konves@gmail.com>
2017-01-04 11:22:37 -0700

    Add express middleware test harness

---
+++ tests/express-test-harness.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const express = require('express');
+const request = require('supertest');
+
+const gangplank = require('../index');
+
+describe('request validator', function () {
+	// SETUP
+	const app = express();
+	app.use(gangplank.middleware({
+		swaggerDefinition: {
+			swagger: '2.0',
+			info: { version: '1.0.0', title: 'TEST' },
+			paths: {
+				'/test': {
+					get: {
+						responses: { '200': { description: 'test' } },
+						parameters: [
+							{
+								name: 'q',
+								in: 'query',
+								description: 'name',
+								type: 'string',
+								required: true
+							}
+						]
+					}
+				}
+			}
+		}
+	}));
+	app.get('/test', (req, res) => {
+		res.sendStatus(200);
+	});
+	app.use(gangplank.errorHandler);
+
+	it('permits valid request', function (done) {
+		// ARRANGE
+		const expectedStatusCode = 200;
+
+		// ACT
+		request(app).get('/test?q=asdf').end((err, res) => {
+
+			// ASSERT
+			assert.notOk(err);
+			assert.ok(res.body);
+			assert.equal(res.statusCode, expectedStatusCode);
+
+			done();
+		})
+	});
+
+	it('rejects request for undefined route', function (done) {
+		// ARRANGE
+		const expectedStatusCode = 404;
+
+		// ACT
+		request(app).get('/undefined-route').end((err, res) => {
+
+			// ASSERT
+			assert.notOk(err);
+			assert.ok(res.body);
+			assert.equal(res.statusCode, expectedStatusCode);
+
+			done();
+		})
+	});
+
+	it('rejects request missing required parameter', function (done) {
+		// ARRANGE
+		const expectedStatusCode = 400;
+
+		// ACT
+		request(app).get('/test').end((err, res) => {
+
+			// ASSERT
+			assert.notOk(err);
+			assert.ok(res.body);
+			assert.equal(res.statusCode, expectedStatusCode);
+
+			done();
+		})
+	});
+});
