ccf9b4deb19f9fb8c986e75e7e402f1ea87edf62
Steve Konves <stephen.konves@gmail.com>
2017-03-07 20:50:46 -0700

    add datetime test harness

---
+++ tests/express-date-test-harness.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('supertest');
+
+const gangplank = require('../index');
+
+function clone(obj) {
+	return JSON.parse(JSON.stringify(obj));
+}
+
+describe('date and datetime tests', function () {
+	// SETUP
+	const swaggerDefinition = {
+		swagger: '2.0',
+		info: { version: '1.0.0', title: 'TEST' },
+		paths: {
+			'/test': {
+				get: {
+					parameters: [
+						{
+							name: 'date',
+							in: 'query',
+							description: 'name',
+							type: 'string',
+							format: 'date',
+							required: false
+						},
+						{
+							name: 'datetime',
+							in: 'query',
+							description: 'name',
+							type: 'string',
+							format: 'date-time',
+							required: false
+						}
+					],
+					responses: { 'default': { description: 'test' } }
+				}
+			}
+		}
+	};
+
+	it('parses and casts valid date', function (done) {
+		// ARRANGE
+		const app = express();
+		app.use(bodyParser.json());
+		app.use(gangplank.requests({ swaggerDefinition }));
+		app.get('/test', (req, res) => {
+
+			// ASSERT
+			assert.isTrue(req.gangplank.params.date instanceof Date);
+			assert.isTrue(req.gangplank.params.datetime instanceof Date);
+			res.send();
+		});
+
+		// ACT
+		request(app).get('/test?date=2016-07-11&datetime=2016-07-11T11:22:33.444Z').end((err, res) => done())
+	});
+});
